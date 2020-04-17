10cea09b326a10b47360836e013336d11884d9a0
Elina Lua Ming <ardentlycurious101@gmail.com>
2020-04-17 00:52:57 -0700

    second commit
    updated POST buttons for work and etc. also updated GET buttons to make list responsive to categories (work or etc.)

--- app.js
+++ app.js
@@ -1,36 +1,61 @@
 const bodyParser = require("body-parser");
-let express = require('express');
+const express = require('express');
+const date = require(__dirname + "/date.js");
 let app = express();
 
-var items = [];
+var workItems = ["code", "exercise"];
+var etcItems = ["shower Brian with love"];
+const today = date.getDate();
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
-
 app.set('view engine', 'ejs');
 
-app.get("/", (req, res) => {
-  const today = new Date();
-  const currentDay = today.getDay();
-  const options = {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric"
-  }
-
-  res.render('list', {
-    day: today.toLocaleDateString("en-US", options),
-    itemsList: items
+app.get("/etc", (req, res) => {
+  res.render("list", {
+    itemsList: etcItems,
+    day: today,
+    category: "etc"
   });
 });
 
-app.post("/", (req, res) => {
+app.get("/", (req, res) => {
+  res.render("list", {
+    itemsList: workItems,
+    day: today,
+    category: "work"
+  });
+});
+
+app.get("/work", (req, res) => {
+  res.redirect("/")
+});
+
+app.post("/work", (req, res) => {
+  console.log(req.body);
   var item = req.body.newItem;
-  items.push(item);
-  res.redirect("/");
+  workItems.push(item);
+  res.redirect("/work");
+});
+
+app.post("/etc", (req, res) => {
+  console.log(req.body);
+  var item = req.body.newItem;
+  etcItems.push(item);
+  res.redirect("/etc");
 });
 
 app.listen(3000, function() {
   console.log("Server is up and running on port 3000.");
 });
+
+function getAllItems(list1, list2) {
+  let newList = []
+  for (var i=0; i<list1.length; i++) {
+    newList.push(list1[i]);
+  }
+  for (var i=0; i<list2.length; i++) {
+    newList.push(list2[i]);
+  }
+  return newList;
+}
