e107a38656edbe78b85064a401dd5c6f76a6d82d
Elina Lua Ming <ardentlycurious101@gmail.com>
2020-04-19 16:33:52 -0700

    third commit: refactored code, make todolist deletable, hooked up mongoDB Atlas.

--- app.js
+++ app.js
@@ -1,61 +1,121 @@
 const bodyParser = require("body-parser");
 const express = require('express');
-const date = require(__dirname + "/date.js");
-const app = express();
+const date = require(__dirname + "/helper_functions/date.js");
+const mongoose = require("mongoose");
 
-const workItems = ["code", "exercise"];
-const etcItems = ["shower Brian with love"];
-const today = date.getDate();
+const app = express();
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
-app.get("/etc", (req, res) => {
-  res.render("list", {
-    itemsList: etcItems,
-    day: today,
-    category: "etc"
+const today = date.getDate();
+
+/* ------------------------------------ Set up database ------------------------------------ */
+
+mongoose.connect("mongodb+srv://elinaluaming:test123@todolist-r7x81.mongodb.net/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const workSchema = {
+  name: String,
+  checklist: Boolean
+};
+
+const etcSchema = {
+  name: {
+    type: String,
+    required: true
+  },
+  checklist: {
+    type: Boolean,
+    required: true
+  }
+};
+
+const Work = mongoose.model("workItem", workSchema);
+
+const Etc = mongoose.model("etcItem", etcSchema);
+
+/* ------------------------------------ APIs ------------------------------------ */
+
+app.route("/")
+  .get((req, res) => {
+    res.redirect("/work");
   });
-});
 
-app.get("/", (req, res) => {
-  res.render("list", {
-    itemsList: workItems,
-    day: today,
-    category: "work"
+app.route("/:itemCategory")
+  .get((req, res) => {
+    const category = req.params.itemCategory;
+
+    if ( category === "work") {
+      const etc = Work.find((err, foundItems) => {
+        res.render("list", {
+          itemsList: foundItems,
+          day: today,
+          category: category
+        });
+      })
+    } else if (category === "etc"){
+      const etc = Etc.find((err, foundItems) => {
+        res.render("list", {
+          itemsList: foundItems,
+          day: today,
+          category: category
+        });
+      })
+    }
+  })
+
+  .post((req, res) => {
+    const category = req.params.itemCategory;
+
+    if (category === "work") {
+      const newItem = new Work({
+        name: req.body.newItem,
+        checklist: false
+      })
+
+      newItem.save((err, item) => {
+        if (!err) { console.log("Successfully posted " + item.name + "!");}
+          else { console.error(err);}
+      })
+
+      res.redirect("/work");
+    } else if (category === "etc") {
+      const newItem = new Etc({
+        name: req.body.newItem,
+        checklist: false
+      })
+
+      newItem.save((err, item) => {
+        if (!err) { console.log("Successfully posted " + item.name + "!");}
+          else { console.error(err);}
+      })
+
+      res.redirect("/etc");
+    }
   });
-});
 
-app.get("/work", (req, res) => {
-  res.redirect("/")
-});
+app.route("/delete/:itemCategory")
 
-app.post("/work", (req, res) => {
-  console.log(req.body);
-  const item = req.body.newItem;
-  workItems.push(item);
-  res.redirect("/work");
-});
+  .post((req, res) => {
+    const checkedItemID = req.body.checkbox;
+    const itemCategory = req.params.itemCategory;
 
-app.post("/etc", (req, res) => {
-  console.log(req.body);
-  const item = req.body.newItem;
-  etcItems.push(item);
-  res.redirect("/etc");
-});
+    if (itemCategory === "work") {
+      Work.findByIdAndDelete(checkedItemID, (err, item) => {
+        if (!err) { console.log("Successfully deleted \"" + item.name + "\"!");}
+      })
+    } else {
+      Etc.findByIdAndDelete(checkedItemID, (err, item) => {
+        if (!err) { console.log("Successfully deleted \"" + item.name + "\"!");}
+      })
+    }
+
+    res.redirect("/" + itemCategory);
+
+  });
+/* ------------------------------------ Others ------------------------------------ */
 
 app.listen(3000, function() {
   console.log("Server is up and running on port 3000.");
 });
-
-function getAllItems(list1, list2) {
-  const newList = []
-  for (var i=0; i<list1.length; i++) {
-    newList.push(list1[i]);
-  }
-  for (var i=0; i<list2.length; i++) {
-    newList.push(list2[i]);
-  }
-  return newList;
-}
